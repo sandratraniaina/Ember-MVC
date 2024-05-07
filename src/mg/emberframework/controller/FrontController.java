2c461c60e86426d6d19902f93c507f42f451193f
sandratraniaina <sandratrarafa@gmail.com>
2024-05-07 11:25:02 +0300

    feat: Override base method for FrontController

--- src/mg/emberframework/controller/FrontController.java
+++ src/mg/emberframework/controller/FrontController.java
@@ -1,7 +1,26 @@
 package mg.emberframework.controller;
 
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 public class FrontController extends HttpServlet {
+
+    private void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
 
 }
