a519ea522d421b500636c1fef83ac6216a0044f8
sandratraniaina <sandratrarafa@gmail.com>
2024-05-19 18:11:37 +0300

    feat: Create Mapping base class

---
+++ src/mg/emberframework/url/Mapping.java
@@ -0,0 +1,29 @@
+package mg.emberframework.url;
+
+public class Mapping {
+    String className, methodName;
+
+    // Construtors
+    public Mapping() {}
+    public Mapping(String className, String methodName) {
+        setClassName(className);
+        setMethodName(methodName);
+    }
+
+    // Getters and setters
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getMethodName() {
+        return methodName;
+    }
+
+    public void setMethodName(String methodName) {
+        this.methodName = methodName;
+    }
+}
