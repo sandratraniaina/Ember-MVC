72e1e177ef1dade430ca62778f5468f6dc5ed147
sandratraniaina <sandratrarafa@gmail.com>
2024-05-28 13:56:04 +0300

    feat: Create paragraph method

--- src/mg/emberframework/util/TagBuilder.java
+++ src/mg/emberframework/util/TagBuilder.java
@@ -1,17 +1,20 @@
 package mg.emberframework.util;
 
 public class TagBuilder {
-    public static String enclose(String tag, String text) {
+    public static String enclose(String text, String tag) {
         return "<" + tag + ">" + text + "</" + tag + ">";
     }
 
     public static String bold(String text) {
-        return enclose("strong", text);
+        return enclose(text, "strong");
     }
 
     public static String heading(String text, int level) {
         String tag = "h" + level;
-        return enclose(tag, text);
+        return enclose(text, tag);
     }
     
+    public static String paragraph(String text) {
+        return enclose(text, "p");
+    }
 }
