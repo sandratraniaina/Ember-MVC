8caf4d849673a003d8082670330984c27d20dcef
sandratraniaina <sandratrarafa@gmail.com>
2024-05-28 10:45:59 +0300

    refactor: Create executeMethod and getArgsClasses methods

--- src/mg/emberframework/util/ReflectUtils.java
+++ src/mg/emberframework/util/ReflectUtils.java
@@ -1,5 +1,24 @@
 package mg.emberframework.util;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class ReflectUtils {
-    
+    public static Class<?>[] getArgsClasses(Object... args) {
+        Class<?>[] classes = new Class[args.length];
+        int i = 0;
+
+        for (Object object : args) {
+            classes[i] = object.getClass();
+            i++;
+        }
+
+        return classes;
+    }
+
+    public static Object executeMethod(Object object, String methodName, Object... args) throws NoSuchMethodException,
+            SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+        Method method = object.getClass().getMethod(methodName, getArgsClasses(args));
+        return method.invoke(object, args);
+    }
 }
