3728dbdb74f5e8bf6e34b319a253eb065ce17234
sandratraniaina <sandratrarafa@gmail.com>
2024-06-03 17:49:00 +0300

    feat: Create IllegalReturnTypeException class

---
+++ src/mg/emberframework/manager/exception/IllegalReturnTypeException.java
@@ -0,0 +1,7 @@
+package mg.emberframework.manager.exception;
+
+public class IllegalReturnTypeException extends Exception{
+    public IllegalReturnTypeException(String message) {
+        super(message);
+    }
+}
