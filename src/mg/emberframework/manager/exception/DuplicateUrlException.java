9aea408385f2e097c7a7531e188507bd2ecbea00
sandratraniaina <sandratrarafa@gmail.com>
2024-06-06 22:03:37 +0300

    feat: Create DuplicateUrlException class

---
+++ src/mg/emberframework/manager/exception/DuplicateUrlException.java
@@ -0,0 +1,7 @@
+package mg.emberframework.manager.exception;
+
+public class DuplicateUrlException extends Exception{
+    public DuplicateUrlException(String message) {
+        super(message);
+    }
+}
