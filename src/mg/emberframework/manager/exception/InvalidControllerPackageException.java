507dceb3155653f2be8538c8cb844bd1105b0c0a
sandratraniaina <sandratrarafa@gmail.com>
2024-06-06 22:05:43 +0300

    feat: Create InvalidControllerPackageException

---
+++ src/mg/emberframework/manager/exception/InvalidControllerPackageException.java
@@ -0,0 +1,7 @@
+package mg.emberframework.manager.exception;
+
+public class InvalidControllerPackageException extends Exception{
+    public InvalidControllerPackageException(String message) {
+        super(message);
+    }
+}
