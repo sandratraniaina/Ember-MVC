0a73ca5e0fc181d2f422ff7958fc3e4a41f91e62
sandratraniaina <sandratrarafa@gmail.com>
2024-06-11 14:08:27 +0300

    feat: Create getMethodParameterAnnotationValue method

--- src/mg/emberframework/util/ReflectUtils.java
+++ src/mg/emberframework/util/ReflectUtils.java
@@ -2,8 +2,27 @@ package mg.emberframework.util;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+import mg.emberframework.annotation.RequestParameter;
 
 public class ReflectUtils {
+    public List<String> getMethodParameterAnnotationValue(Method method) {
+        List<String> values = new ArrayList<>();
+
+        for (Class<?> clazz : method.getParameterTypes()) {
+            if (clazz.isAnnotationPresent(RequestParameter.class)) {
+                RequestParameter annotation = clazz.getAnnotation(RequestParameter.class);
+                values.add(annotation.value());
+            } else {
+                values.add(null);
+            }
+        }
+        
+        return values;
+    }
+
     public static Class<?>[] getArgsClasses(Object... args) {
         Class<?>[] classes = new Class[args.length];
         int i = 0;
