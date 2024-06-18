535adad9b566514069f61575e91096455bc343dc
sandratraniaina <sandratrarafa@gmail.com>
2024-06-18 09:28:32 +0300

    feat: Create isPrimitive method in ObjectUtils

--- src/mg/emberframework/util/ObjectUtils.java
+++ src/mg/emberframework/util/ObjectUtils.java
@@ -2,9 +2,21 @@ package mg.emberframework.util;
 
 import java.lang.reflect.InvocationTargetException;
 import java.sql.Date;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
 
 public class ObjectUtils {
+    public static boolean isPrimitive(Class<?> clazz) {
+        List<Class<?>> primitiveTypes = new ArrayList<>();
+        primitiveTypes.add(Integer.TYPE);
+        primitiveTypes.add(Double.TYPE);
+        primitiveTypes.add(String.class);
+
+        return primitiveTypes.contains(clazz);
+    }
+
     public static Object getDefaulValue(Class<?>  clazz) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
         return getDefaulValue(clazz.getConstructor().newInstance());
     }
