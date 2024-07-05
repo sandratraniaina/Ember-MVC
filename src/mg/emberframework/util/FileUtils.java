9e04c445ffc9ccbf5d4c77682cb755e9da5b3043
sandratraniaina <sandratrarafa@gmail.com>
2024-07-05 11:44:26 +0300

    refactor: Add private contructor to FileUtils

--- src/mg/emberframework/util/FileUtils.java
+++ src/mg/emberframework/util/FileUtils.java
@@ -4,4 +4,7 @@ public class FileUtils {
     public static String getSimpleFileName(String fileName, String extension) {
         return fileName.substring(0, (fileName.length() - extension.length()) - 1);
     }
+
+    private FileUtils() {
+    }
 }
