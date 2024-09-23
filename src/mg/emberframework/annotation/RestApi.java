c654a66fd477d6a74a68f8e05df03964f20ca555
sandratraniaina <sandratrarafa@gmail.com>
2024-09-23 09:09:47 +0300

    feat: Create RespApi annotation class

---
+++ src/mg/emberframework/annotation/RestApi.java
@@ -0,0 +1,8 @@
+package mg.emberframework.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RestApi {
+}
