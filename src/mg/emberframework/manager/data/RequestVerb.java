368ddfdaa12b003fcffa372127f6e726d16ddfc6
sandratraniaina <sandratrarafa@gmail.com>
2024-10-09 22:14:05 +0300

    feat: Create getMethodVerb  method in RequestVerb

--- src/mg/emberframework/manager/data/RequestVerb.java
+++ src/mg/emberframework/manager/data/RequestVerb.java
@@ -1,8 +1,18 @@
 package mg.emberframework.manager.data;
 
+import java.lang.reflect.Method;
+
 public class RequestVerb {
     public static final String GET = "GET";
     public static final String POST = "POST";
     public static final String DELETE = "DELETE";
     public static final String PUT = "PUT";
+
+    public static String getMethodVerb(Method method) {
+        String verb = RequestVerb.GET;
+        if (method.isAnnotationPresent(mg.emberframework.annotation.request.POST.class)) {
+            verb = POST;
+        }
+        return verb;
+    }
 }
