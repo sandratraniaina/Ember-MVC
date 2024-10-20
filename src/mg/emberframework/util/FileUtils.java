8833fe1f24109a48a896e8893c11d4fb3dc4945b
sandratraniaina <sandratrarafa@gmail.com>
2024-10-20 20:11:34 +0300

    feat: Create createFilePath method in FileUtils class

--- src/mg/emberframework/util/FileUtils.java
+++ src/mg/emberframework/util/FileUtils.java
@@ -1,6 +1,13 @@
 package mg.emberframework.util;
 
 public class FileUtils {
+    public static String createFilePath(String dirPath, String fileName) {
+        dirPath = dirPath.replaceAll("/+$", "");
+        fileName = fileName.replaceAll("^/+", "");
+
+        return dirPath + "/" + fileName;
+    }
+
     public static String getSimpleFileName(String fileName, String extension) {
         return fileName.substring(0, (fileName.length() - extension.length()) - 1);
     }
