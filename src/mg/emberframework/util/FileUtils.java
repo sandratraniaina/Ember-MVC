532a580cc796ac3623ae50b0b00d653a24a4049e
sandratraniaina <sandratrarafa@gmail.com>
2024-10-20 20:17:57 +0300

    feat: Create getPartByte method in FileUtils

--- src/mg/emberframework/util/FileUtils.java
+++ src/mg/emberframework/util/FileUtils.java
@@ -1,6 +1,26 @@
 package mg.emberframework.util;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import jakarta.servlet.http.Part;
+
 public class FileUtils {
+    public byte[] getPartByte(Part part) throws IOException {
+        try (InputStream inputStream = part.getInputStream();
+                ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+
+            return outputStream.toByteArray();
+        }
+    }
+
     public static String createFilePath(String dirPath, String fileName) {
         dirPath = dirPath.replaceAll("/+$", "");
         fileName = fileName.replaceAll("^/+", "");
