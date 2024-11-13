a58ebafad670aee5c165811766c9ea07cbe36d53
sandratraniaina <sandratrarafa@gmail.com>
2024-11-13 15:05:00 +0300

    feat: Create Required annotation

---
+++ src/mg/emberframework/annotation/validation/Required.java
@@ -0,0 +1,8 @@
+package mg.emberframework.annotation.validation;
+
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Retention;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Required {
+}
