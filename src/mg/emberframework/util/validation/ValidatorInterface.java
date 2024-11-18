ab2f2acd4fb9ce071e852698d26c47e5088ea524
sandratraniaina <sandratrarafa@gmail.com>
2024-11-18 22:19:51 +0300

    fix: Update validate method for Validator

--- src/mg/emberframework/util/validation/ValidatorInterface.java
+++ src/mg/emberframework/util/validation/ValidatorInterface.java
@@ -1,9 +1,10 @@
 package mg.emberframework.util.validation;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 
 import mg.emberframework.manager.exception.ModelValidationException;
 
 public interface ValidatorInterface {
-    public void validate(String value, Field field) throws ModelValidationException;
+    public void validate(String value, Annotation annotation,Field field) throws ModelValidationException;
 }
