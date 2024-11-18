6568b155e2393c8b2f1aa09a0c7a5db468d4d412
sandratraniaina <sandratrarafa@gmail.com>
2024-11-18 22:16:34 +0300

    feat: Add ModelValidationException exception

---
+++ src/mg/emberframework/manager/exception/ModelValidationException.java
@@ -0,0 +1,7 @@
+package mg.emberframework.manager.exception;
+
+public class ModelValidationException extends Exception{
+    public ModelValidationException(String message) {
+        super(message);
+    }
+}
