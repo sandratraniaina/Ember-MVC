ec23d48248ca04a35f3a0f623fd830f6cc145648
sandratraniaina <sandratrarafa@gmail.com>
2024-11-18 22:26:38 +0300

    feat: Implement EmailValidator validate method

--- src/mg/emberframework/util/validation/EmailValidator.java
+++ src/mg/emberframework/util/validation/EmailValidator.java
@@ -9,7 +9,10 @@ public class EmailValidator implements ValidatorInterface{
 
     @Override
     public void validate(String value, Annotation annotation, Field field) throws ModelValidationException {
-        
+        boolean checker = (value).matches("[^@\\s]+@[^@\\s]+\\.[^@\\s]+");
+        if (!checker) {
+            throw new ModelValidationException(field.getName() + " must be a valid email.");
+        }
     }
     
 }
