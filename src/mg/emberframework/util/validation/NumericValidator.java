025c07567e12cb0ade995a0d656938a5d18da983
sandratraniaina <sandratrarafa@gmail.com>
2024-11-18 22:32:02 +0300

    feat: Implement NumericValidator validate method

--- src/mg/emberframework/util/validation/NumericValidator.java
+++ src/mg/emberframework/util/validation/NumericValidator.java
@@ -9,7 +9,9 @@ public class NumericValidator implements ValidatorInterface{
 
     @Override
     public void validate(String value, Annotation annotation, Field field) throws ModelValidationException {
-        
+        if (!(value != null && value.matches("\\d+"))) {
+            throw new ModelValidationException(field.getName() + " should be numeric");
+        }
     }
     
 }
