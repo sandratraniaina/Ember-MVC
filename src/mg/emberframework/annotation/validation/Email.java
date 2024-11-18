fa105bd4c4c1d6e84dd03d3d9ee8e125b32bdb0f
sandratraniaina <sandratrarafa@gmail.com>
2024-11-18 21:21:22 +0300

    feat: Add Email validation annotation

---
+++ src/mg/emberframework/annotation/validation/Email.java
@@ -0,0 +1,9 @@
+package mg.emberframework.annotation.validation;
+
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Retention;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Email {
+    
+}
