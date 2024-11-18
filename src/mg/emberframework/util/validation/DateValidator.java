b9a928c67876ba103c725f6bb2fcb53e9f386097
sandratraniaina <sandratrarafa@gmail.com>
2024-11-18 22:24:10 +0300

    wip: Waiting on implementing DateValidator validate method

--- src/mg/emberframework/util/validation/DateValidator.java
+++ src/mg/emberframework/util/validation/DateValidator.java
@@ -3,13 +3,17 @@ package mg.emberframework.util.validation;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 
+import mg.emberframework.annotation.validation.Date;
 import mg.emberframework.manager.exception.ModelValidationException;
 
 public class DateValidator implements ValidatorInterface{
 
     @Override
     public void validate(String value, Annotation annotation,Field field) throws ModelValidationException {
+        Date dateAnnotation = ((Date)annotation);
+        String format = dateAnnotation.format() != null ? dateAnnotation.format() : "yyyy-mm-dd";
 
+        // TODO: Implement DateValidator logic
     }
     
 }
